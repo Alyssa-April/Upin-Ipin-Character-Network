d1fbb6052fd06ef2b15839821ced4ec0b4369d86
AlyCodes <101617796+Alyssa-April@users.noreply.github.com>
2026-04-21 19:07:03 +0800

    Sample Episodes and Intervals for Re-Annotation
    Five sample episodes and film intervals (each) for re-annotation

---
+++ Sample Episodes and Intervals for Re-Annotation.R
@@ -0,0 +1,24 @@
+set.seed(1234)  # for reproducibility
+
+# Sample 3 episodes from 1–18
+episodes_sample <- sample(1:18, 3, replace = FALSE)
+
+# Sample 3 film intervals from 1–19
+intervals_sample <- sample(1:19, 3, replace = FALSE)
+
+episodes_sample
+intervals_sample
+
+
+set.seed(55)  # for reproducibility
+
+# Sample 2 episodes from 1–18
+episodes_sample <- sample(1:18, 2, replace = FALSE)
+
+# Sample 2 film intervals from 1–19
+intervals_sample <- sample(1:19, 2, replace = FALSE)
+
+episodes_sample
+intervals_sample
+
+
